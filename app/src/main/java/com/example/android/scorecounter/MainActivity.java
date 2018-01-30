9233c688af195510b97e486393e67ad036427db4
Mr.C <cadogantes@gmail.com>
2018-01-30 21:25:01 +0100

    Alpha version. Two notable bugs: sometimes screen becomes unclickable and new images added through "Reset" are animated incorrectly (like the view bounds are wrong)

--- app/src/main/java/com/example/android/scorecounter/MainActivity.java
+++ app/src/main/java/com/example/android/scorecounter/MainActivity.java
@@ -1,88 +1,104 @@
 package com.example.android.scorecounter;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 import org.w3c.dom.Text;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class MainActivity extends Activity {
+import static java.lang.Math.abs;
 
-    ImageView img_animation;
+public class MainActivity extends Activity {
+    public int numberOfImages = 10;
+    public int activeImages = 0;
     TextView tvPearCount, tvBananaCount, tvStrawberryCount;
-    public float x, y, X, Y, dx, dy, xWidth, yHeight;
+    public float[] dx = new float[numberOfImages];       //dx and dy indicate both value (in pixels) and direction in which particular image should be moved
+    public float[] dy = new float[numberOfImages];       //they are arrays as those information need to be stored betweeen each animation frame. Every animated image needs its own set of dx and dy values
     RelativeLayout root;
+    public float X, Y;
     int iPearCount, iBananaCount, iStrawberryCount;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        img_animation = findViewById(R.id.pear_1);
+
+        initialize();     //method where I put variables needing initialization
+
+        createImageView(numberOfImages);
+    }
+
+    public void initialize() {      //method where I put variables needing initialization
         root = findViewById(R.id.main_root);
         tvBananaCount = findViewById(R.id.banana_count);
         tvStrawberryCount = findViewById(R.id.strawberry_count);
         tvPearCount = findViewById(R.id.pear_count);
-
-        dx = 10f;      //dx and dy will determine whether we move towards the higher or lower coordinates
-        dy = 8f;
         iPearCount = 0;
         iStrawberryCount = 0;
         iBananaCount = 0;
 
-        startAnimation(img_animation);  //starts animation of selected view
     }
 
-    public void resetCounts(View view) {
+    public void reset(View view) {
         iPearCount = 0;
         iStrawberryCount = 0;
         iBananaCount = 0;
         actuateCounts();
+        createImageView(numberOfImages - activeImages);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Reset!";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
-    public void testAnimation(View view) {
-        TranslateAnimation animation = new TranslateAnimation(0.0f, 400.0f, 0.0f, 400.0f);          //  new TranslateAnimation(xFrom,xTo, yFrom,yTo)
-        animation.setDuration(1000);
-        animation.setFillAfter(true);
-        img_animation.startAnimation(animation);
-    }
-
-    public void bounceAnimation(View view) {
-        X = root.getWidth();        // get width of layout where we will be moving images
-        Y = root.getHeight();         // get height of layout where we will be moving images
+    private void bounceAnimation(View view, int i) {
+        float x, xWidth, y, yHeight;       //declaring variables here prevents their values from being swapped between different animations
         x = view.getX();
         y = view.getY();
         xWidth = view.getWidth();
         yHeight = view.getHeight();
 
-
-        if ((y <= Y - yHeight && dy > 0) || (y > 0 && dy < 0)) {   //if we are not at the edge value of y
-            y = y + dy;
-        } else if (y > Y - yHeight && dy > 0) {  //if we are at the edge of y
-            dy = -dy;
+        if ((y <= Y - yHeight && dy[i] > 0) || (y >= 0 && dy[i] < 0)) {   //if we are not at the edge value of y
+            y = y + dy[i];
+        } else if (y > Y - yHeight && dy[i] > 0) {  //if we are at the edge of y
+            dy[i] = -dy[i];
             y = Y - yHeight;
-        } else if (y < 0 && dy < 0) {
-            dy = -dy;
+        } else if (y < 0 && dy[i] < 0) {
+            dy[i] = -dy[i];
             y = -y;
+        } else {
+            y = 10;
+            dy[i] = abs(dy[i]);
         }
 
-        if ((x < (X - xWidth) && x >= 0 && dx > 0) || (x >= 0 && dx < 0))  //if we are not at the edge value of x
+        if ((x < (X - xWidth) && x >= 0 && dx[i] > 0) || (x >= 0 && dx[i] < 0))  //if we are not at the edge value of x
         {
-            x = x + dx;
-        } else if (x >= X - xWidth && dx > 0) {  //if we are at the edge of x
-            dx = -dx;
+            x = x + dx[i];
+        } else if (x >= X - xWidth && dx[i] > 0) {  //if we are at the edge of x
+            dx[i] = -dx[i];
             x = (X - xWidth) - (x - (X - xWidth));
-        } else if (x < 0 && dx < 0) {
-            dx = -dx;
+        } else if (x < 0 && dx[i] < 0) {
+            dx[i] = -dx[i];
             x = -x;
+        } else {
+            x = 10;
+            dx[i] = abs(dx[i]);
         }
 
         view.setX(x);
@@ -90,18 +106,13 @@ public class MainActivity extends Activity {
 
     }
 
-    public void testSet(View view) {
-        img_animation.setX(img_animation.getX() + 100f);
-        img_animation.setY(img_animation.getY() + 100f);
-    }
-
-    public void startAnimation(final View view) {
+    public void startAnimation(final View view, final int viewId) {
         Timer t = new Timer();
 
         t.scheduleAtFixedRate(
                 new TimerTask() {
                     public void run() {
-                        bounceAnimation(view);
+                        bounceAnimation(view, viewId);
                     }
                 },
                 0,      // run first occurrence immediatetly
@@ -117,15 +128,65 @@ public class MainActivity extends Activity {
     public void countPears(View view) {
         iPearCount++;
         actuateCounts();
+        activeImages--;
+        view.setVisibility(View.GONE);
     }
 
     public void countBananas(View view) {
         iBananaCount++;
         actuateCounts();
+        activeImages--;
+        view.setVisibility(View.GONE);
     }
 
     public void countStrawberries(View view) {
         iStrawberryCount++;
         actuateCounts();
+        activeImages--;
+        view.setVisibility(View.GONE);
+    }
+
+    public void createImageView(int nrOfImages) {
+        Random r = new Random();
+        int randomInt;
+        for (int i = 0; i < nrOfImages; i++) {
+            ImageView image = new ImageView(this);
+            activeImages++;
+            randomInt = r.nextInt(100);
+            //we have 3 types of fruits, new image will be assigned one of them randomly using result of %3
+            if (randomInt % 3 == 0) {
+                image.setImageResource(R.drawable.strawberry);
+                image.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        countStrawberries(v);
+                    }
+                });
+            } else if (randomInt % 3 == 1) {
+                image.setImageResource(R.drawable.banana);
+                image.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        countBananas(v);
+                    }
+                });
+            } else if (randomInt % 3 == 2) {
+                image.setImageResource(R.drawable.pear);
+                image.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        countPears(v);
+                    }
+                });
+            } else
+                image.setImageResource(R.drawable.football);    //football on screen will mean that something is wrong with randomInt generated;
+
+            dx[i] = r.nextFloat() * 10 + 5;     //set random start values to move vectors
+            dy[i] = r.nextFloat() * 10 + 5;
+            // Adds the view to the layout
+            image.setX(r.nextFloat() * 800);
+            image.setY(r.nextFloat() * 800);
+            root.addView(image);
+
+            //start view animation
+            startAnimation(image, i);
+        }
     }
 }
